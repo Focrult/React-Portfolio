9f33c330af766d863607c118bb62a66e1a645935
Focrult <focrult@gmail.com>
2023-04-20 17:21:42 +0800

    Adjustments to final files, deleted utils folder - not relevant

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,20 +6,15 @@ const About = () => {
 
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">   
             
-            <div className="pb-8">
+            <div className="pb-5">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
-            <p className="text-xl mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sit 
-            laboriosam illo at, repellat itaque nihil veniam minima reiciendis facere doloremque ratione 
-            excepturi? Veritatis quia vero ex omnis ipsa hic libero. Ea consequatur sapiente, quas id expedit
-            a molestiae maxime at nisi sit autem harum, 
-            distinctio eos alias molestias architecto recusandae.</p>
+            <p className="text-xl mt-10">I am currently a student who is studying web development and cyber security. My goal for my future career is to pursue web development and help the industry grow in a positive way.
+            I enjoy solving problems, learning from other like minded people when it comes to designing and programming complex back-end and front-end.</p>
         <br />
-
         <p className="text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minima sint hic soluta eligendi iusto quibusdam laudantium harum, consectetur consequuntur esse delectus accusantium maiores quasi id corporis ullam sed 
-            ab nisi sequi neque. Mollitia, minima tenetur eaque ipsam omnis repellat 
-            cum doloribus facilis excepturi provident itaque, cupiditate, harum distinctio placeat.</p>
+        I have completed a number of projects and challenges throughout this course and in my own personal time. Trying to learn a large array of technologies to stand out. It would be great if you could take to time to review my projects!    
+        </p>
         
         </div>
     </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import {useState} from 'react';
 
-
-
-
 const Contact = () => {
     
 
